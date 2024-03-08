3f90991c2e5d785dd868312a0d822a31239976aa
Sacintha Raj <sacintha.raj@digital.justice.gov.uk>
2024-03-08 16:27:10 +0000

    fixed the test issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,6 +100,9 @@ java {
   targetCompatibility = JavaVersion.VERSION_21
 }
 
+repositories {
+  mavenCentral()
+}
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
