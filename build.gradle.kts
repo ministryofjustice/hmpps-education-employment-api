83c6d4075b1239c3c4637e7522665488f0d3d5ad
Sacintha Raj <sacintha.raj@digital.justice.gov.uk>
2024-07-03 15:54:37 +0100

    removed a security vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,23 @@ plugins {
   kotlin("plugin.jpa") version "2.0.0"
   id("jacoco")
 }
-
+configurations {
+  implementation {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
+  testImplementation {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
+}
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1") {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0") {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
   implementation("com.fasterxml.jackson.core:jackson-databind")
   implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
@@ -21,9 +32,15 @@ dependencies {
   testImplementation("org.testcontainers:localstack")
   testImplementation("org.testcontainers:postgresql")
   testImplementation("org.testcontainers:testcontainers")
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
-  testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  developmentOnly("org.springframework.boot:spring-boot-devtools") {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
+  testImplementation("org.springframework.security:spring-security-test") {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5") {
+    exclude(group = "org.xmlunit", module = "xmlunit-core")
+  }
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
   testImplementation("com.h2database:h2")
 }
@@ -38,8 +55,12 @@ testing {
       dependencies {
         testType.set(TestSuiteType.INTEGRATION_TEST)
         kotlin.target.compilations { named("integrationTest") { associateWith(getByName("main")) } }
-        implementation("org.springframework.boot:spring-boot-starter-test")
-        implementation("org.springframework.security:spring-security-test")
+        implementation("org.springframework.boot:spring-boot-starter-test") {
+          exclude(group = "org.xmlunit", module = "xmlunit-core")
+        }
+        implementation("org.springframework.security:spring-security-test") {
+          exclude(group = "org.xmlunit", module = "xmlunit-core")
+        }
         implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
         implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
         implementation("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
