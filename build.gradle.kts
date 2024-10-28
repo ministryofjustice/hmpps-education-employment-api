18c50a022ce42e8abe1686b547fd938328a52f33
Rick Choi <rick.choi@digital.justice.gov.uk>
2024-10-28 10:52:11 +0000

    [ESWE-1109] Upgrade Spring Boot Upgrade framework and dependencies for resolving vulnerabilities. (#63)
    Upgrade framework and dependencies for resolving vulnerabilities.
    
    - upgrade Spring Boot to `3.3.5` (and Spring Framework `6.1.14`); (was
      `3.3.4` and `6.1.13`)
    - upgrade Kotlin to `2.0.21` (was `2.0.20`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
-  kotlin("plugin.spring") version "2.0.20"
-  kotlin("plugin.jpa") version "2.0.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  kotlin("plugin.spring") version "2.0.21"
+  kotlin("plugin.jpa") version "2.0.21"
   id("jacoco")
 }
 
