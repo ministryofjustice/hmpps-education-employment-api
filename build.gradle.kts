37c2a9426235dde081b4c9ea3f6c190561f8710a
rickchoijd <rick.choi@digital.justice.gov.uk>
2024-12-30 12:53:15 +0000

    [ESWE-1199] Upgrade dependencies for resolving vulnerabilities.
    upgrade spring boot to `3.4.1` (HMPPS spring boot gradle plugin `6.1.2`, was `6.0.8`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
   kotlin("plugin.spring") version "2.0.21"
   kotlin("plugin.jpa") version "2.0.21"
   id("jacoco")
 }
 
+ext["logback.version"] = "1.5.15"
+
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.1") {
-    implementation("io.netty:netty-common:4.1.115.Final")
     implementation("org.apache.commons:commons-compress:1.27.1")
-    testImplementation("org.xmlunit:xmlunit-core:2.10.0")
   }
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
   implementation("com.fasterxml.jackson.core:jackson-databind")
   implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
@@ -40,9 +40,7 @@ testing {
       dependencies {
         testType.set(TestSuiteType.INTEGRATION_TEST)
         kotlin.target.compilations { named("integrationTest") { associateWith(getByName("main")) } }
-        implementation("org.springframework.boot:spring-boot-starter-test") {
-          implementation("org.xmlunit:xmlunit-core:2.10.0")
-        }
+        implementation("org.springframework.boot:spring-boot-starter-test")
         implementation("org.springframework.security:spring-security-test")
         implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
         implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
