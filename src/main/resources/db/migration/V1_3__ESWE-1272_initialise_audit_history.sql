9e10bf8e807061b92fd268404fd29727ed5dc181
Rick Choi <rick.choi@digital.justice.gov.uk>
2025-03-27 10:07:41 +0000

    [ESWE-1272] Initialise audit history of readiness profile (#73)

---
+++ src/main/resources/db/migration/V1_3__ESWE-1272_initialise_audit_history.sql
@@ -0,0 +1,11 @@
+-- initialise revision/audit tables
+DELETE FROM revision_info;
+DELETE FROM work_readiness_audit;
+
+SELECT setval('revision_info_rev_number_seq', 1, true);
+
+INSERT INTO revision_info
+SELECT currval('revision_info_rev_number_seq') as rev_number, date_part('epoch', now()) * 1000, 'SYSTEM';
+
+insert into work_readiness_audit
+select currval('revision_info_rev_number_seq'), 0, wr.* from work_readiness wr where offender_id not in (select distinct offender_id from work_readiness_audit);
