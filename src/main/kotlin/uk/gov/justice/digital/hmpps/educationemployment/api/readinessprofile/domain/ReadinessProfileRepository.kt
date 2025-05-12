f1564a69d77568a70036029e2eb3c617fd3d6202
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-05-12 18:07:53 +0100

    [ESWE-1153] Dashboard API - Reasons for support declined
    revise query, for within12Weeks aggregation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationemployment/api/readinessprofile/domain/ReadinessProfileRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationemployment/api/readinessprofile/domain/ReadinessProfileRepository.kt
@@ -15,19 +15,32 @@ interface ReadinessProfileRepository :
   @Query(
     """
     WITH recent_updates AS (
-      SELECT rev_number, offender_id FROM work_readiness_audit wra
+      SELECT rev_number, offender_id, profile_data FROM work_readiness_audit wra
       WHERE wra.modified_date_time BETWEEN :startTime AND :endTime AND wra.rev_type IN (0,1)
+    ), profile_at_start AS (
+      SELECT wra1.rev_number, wra1.offender_id, wra1.profile_data FROM work_readiness_audit wra1
+      INNER JOIN (
+        SELECT offender_id, max(rev_number) as rev_number 
+        FROM work_readiness_audit wra2
+        WHERE wra2.offender_id IN (SELECT DISTINCT offender_id from recent_updates) AND wra2.modified_date_time <= :startTime AND wra2.rev_type IN (0,1)
+        GROUP BY 1
+      ) at_start 
+      ON wra1.offender_id = at_start.offender_id AND at_start.rev_number = at_start.rev_number
+      AND at_start.rev_number NOT IN (SELECT rev_number from recent_updates)
+    ), profile_snapshots AS (
+      SELECT * FROM recent_updates UNION ALL SELECT * FROM profile_at_start
     ), latest AS (
-      SELECT offender_id, MAX(rev_number) AS rev_number FROM recent_updates GROUP BY offender_id
+      SELECT offender_id, MAX(rev_number) AS rev_number, BOOL_OR(COALESCE((profile_data->>'within12Weeks')::boolean, true)) as within_12_weeks
+      FROM profile_snapshots GROUP BY offender_id
     ), latest_profiles AS (
-      SELECT wra.offender_id, 
-        wra.profile_data->>'prisonId' AS prison_id,
-        COALESCE((wra.profile_data->>'within12Weeks')::boolean, true) AS within_12_weeks,
-        wra.profile_data#>'{supportDeclined, supportToWorkDeclinedReason}' AS declined_reasons
-      FROM work_readiness_audit wra INNER JOIN latest l
-      ON L.offender_id = wra.offender_id AND l.rev_number = wra.rev_number
-      WHERE wra.profile_data->>'status' = 'SUPPORT_DECLINED'
-      AND wra.profile_data->>'prisonId' = :prisonId
+      SELECT p.offender_id, 
+        p.profile_data->>'prisonId' AS prison_id,
+        l.within_12_weeks,
+        p.profile_data#>'{supportDeclined, supportToWorkDeclinedReason}' AS declined_reasons
+      FROM profile_snapshots p INNER JOIN latest l
+      ON L.offender_id = p.offender_id AND l.rev_number = p.rev_number
+      WHERE p.profile_data->>'status' = 'SUPPORT_DECLINED'
+      AND p.profile_data->>'prisonId' = :prisonId
     ), declined_reasons AS (
       SELECT p.offender_id, p.within_12_weeks, dr.value AS declined_reason FROM latest_profiles p, jsonb_array_elements_text(declined_reasons) AS dr
     )
