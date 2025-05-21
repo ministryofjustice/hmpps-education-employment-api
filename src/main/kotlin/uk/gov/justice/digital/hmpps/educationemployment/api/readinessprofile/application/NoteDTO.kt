3d13536d470259ef4d1df793735b8ac0de00783b
Rick Choi <rick.choi@digital.justice.gov.uk>
2025-05-21 09:22:20 +0100

    [ESWE-1321] remove known names from API spec (and tests) (#88)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationemployment/api/readinessprofile/application/NoteDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationemployment/api/readinessprofile/application/NoteDTO.kt
@@ -5,7 +5,7 @@ import uk.gov.justice.digital.hmpps.educationemployment.api.notesdata.domain.Not
 import java.time.LocalDateTime
 
 data class NoteDTO(
-  @Schema(description = "userId of the note creator", example = "whilesp")
+  @Schema(description = "userId of the note creator", example = "user4")
   val createdBy: String,
 
   @Schema(description = "Created date time", type = "string", pattern = "yyyy-MM-dd'T'HH:mm:ss", example = "2018-12-01T13:45:00", required = true)
