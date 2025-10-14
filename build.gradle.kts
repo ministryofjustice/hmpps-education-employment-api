0c83d5a307d7f3c29753e3dfa2eb52d2bb0fa111
Rick Choi <rick.choi@justice.gov.uk>
2025-10-14 12:45:23 +0100

    [ESWE-1480] Upgrade dependencies (#110)
    - plugin `gradle-spring-boot` to `9.1.3` (was `9.1.1`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.3"
   kotlin("plugin.spring") version "2.2.20"
   kotlin("plugin.jpa") version "2.2.20"
   id("jacoco")
