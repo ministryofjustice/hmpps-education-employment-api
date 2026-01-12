cb1ff1b79ebd9c96c78507d4a0e1cbc41ae20b91
Zahid Khan <Zahid.Khan1@justice.gov.uk>
2026-01-12 15:12:49 +0000

    [ESWE-1602] Bump springboot plugin to 9.3.0 (#116)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.3.0"
   kotlin("plugin.spring") version "2.2.21"
   kotlin("plugin.jpa") version "2.2.21"
   id("jacoco")
