5dbe6f1f73282ca143f98d7e8120bd2b98a047ff
Rick Choi <rick.choi@justice.gov.uk>
2026-04-02 15:33:59 +0100

    ESWE-1807: Update dependencies, remove Trivy (#129)
    Update plugin/dependencies
    - HMPPS Gradle Spring Boot plugin to `10.1.2`
    - Kotlin to `2.3.20`
    - Spring Boot to `4.0.5`
    - HMPPS Kotlin lib to `2.1.0`
    - HMPPS SAR lib to `2.1.3`
    - Gradle wrapper to `9.4.1`
    
    and more:
    - `springdoc-openapi-starter-webmvc-ui` to `3.0.2`
    - `mockito-kotlin` to `6.3.0`
    
    Update Helm chart
    - `generic-prometheus-alerts` to `1.15`
    - `generic-service`	to `3.12`
    
    Remove Trivy scan
    Remove jjwt dependency
    
    replaced customised `JwtAuthHelper` with `JwtAuthorisationHelper` (from
    lib)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,22 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.4"
-  kotlin("plugin.spring") version "2.3.10"
-  kotlin("plugin.jpa") version "2.3.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.2"
+  kotlin("plugin.spring") version "2.3.20"
+  kotlin("plugin.jpa") version "2.3.20"
   id("jacoco")
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.1.0")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
   implementation("org.springframework.data:spring-data-envers")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
   implementation("com.fasterxml.jackson.core:jackson-databind")
-  testImplementation("io.jsonwebtoken:jjwt-api:0.12.6")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   runtimeOnly("org.postgresql:postgresql")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.1")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.0")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -36,8 +35,8 @@ testing {
     val integrationTest by registering(JvmTestSuite::class) {
       dependencies {
         kotlin.target.compilations { named("integrationTest") { associateWith(getByName("main")) } }
-        implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.1")
-        implementation("uk.gov.justice.service.hmpps:hmpps-subject-access-request-test-support:2.1.1")
+        implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.0")
+        implementation("uk.gov.justice.service.hmpps:hmpps-subject-access-request-test-support:2.1.3")
         implementation("org.springframework.boot:spring-boot-starter-test")
         implementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
         implementation("org.springframework.boot:spring-boot-restclient")
@@ -47,11 +46,7 @@ testing {
         runtimeOnly("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
         runtimeOnly("org.flywaydb:flyway-database-postgresql")
         implementation("org.testcontainers:testcontainers-postgresql")
-        implementation("org.mockito.kotlin:mockito-kotlin:6.2.3")
-
-        implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-        runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-        runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+        implementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
       }
 
       targets {
