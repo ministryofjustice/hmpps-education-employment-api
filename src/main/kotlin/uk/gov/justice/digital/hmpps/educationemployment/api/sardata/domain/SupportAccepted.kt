975aa10c3af3c46faf36266ad995f39513e45fb8
rickchoijd <rick.choi@justice.gov.uk>
2026-04-14 15:01:10 +0100

    ESWE-1814: Fix Optional fields of API requests/responses (profile, SAR)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationemployment/api/sardata/domain/SupportAccepted.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationemployment/api/sardata/domain/SupportAccepted.kt
@@ -2,23 +2,22 @@ package uk.gov.justice.digital.hmpps.educationemployment.api.sardata.domain
 
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.educationemployment.api.profiledata.application.instantFromZone
-import uk.gov.justice.digital.hmpps.educationemployment.api.shared.application.ModificationAudited
 import java.time.Instant
 import java.time.ZoneId
 import uk.gov.justice.digital.hmpps.educationemployment.api.profiledata.domain.SupportAccepted as SupportAcceptedEntity
 
 @Schema(name = "SARSupportAccepted", description = "Support accepted of the SAR Readiness Profile")
 data class SupportAccepted(
-  override val modifiedBy: String,
-  override val modifiedDateTime: Instant,
+  val modifiedBy: String?,
+  val modifiedDateTime: Instant?,
   val actionsRequired: ActionsRequired,
   val workImpacts: WorkImpacts,
   val workInterests: WorkInterests,
   val workExperience: WorkExperience,
-) : ModificationAudited {
+) {
   constructor(entity: SupportAcceptedEntity, timeZoneId: ZoneId) : this(
-    modifiedBy = entity.modifiedBy!!,
-    modifiedDateTime = entity.modifiedDateTime!!.instantFromZone(timeZoneId),
+    modifiedBy = entity.modifiedBy,
+    modifiedDateTime = entity.modifiedDateTime?.instantFromZone(timeZoneId),
     actionsRequired = ActionsRequired(entity.actionsRequired, timeZoneId),
     workImpacts = WorkImpacts(entity.workImpacts, timeZoneId),
     workInterests = WorkInterests(entity.workInterests, timeZoneId),
